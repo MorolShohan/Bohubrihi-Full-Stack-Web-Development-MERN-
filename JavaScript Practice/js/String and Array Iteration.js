1752150c5541dde985a8035013dfb4d8971f52d2
MorolShohan <shohan.aiubcse@gmail.com>
2024-05-26 17:25:14 +0600

    solved some problems and learned loops

---
+++ JavaScript Practice/js/String and Array Iteration.js
@@ -0,0 +1,14 @@
+let name = "Shohan!";
+let len = name.length;
+let food = ["cake","chocolate","biscuit","cookies"];
+
+for(var i =0; i<len;i++)
+    {
+        console.log(`index: ${i}`);
+        console.log(name[i]);
+    }
+for(var j =0; j<food.length;j++)
+    {
+        console.log(`index: ${j}`);
+        console.log(food[j]);
+    }
--- JavaScript Practice/js/calculator.js
+++ JavaScript Practice/js/calculator.js
@@ -1,1 +1,29 @@
-console.log()
+console.log("select and option\n1. ADD\n2. Subtract\n3. Multiply\n4. Divide")
+var num1 = prompt("Enter first number: ");
+var num2 = prompt("Enter second number: ");
+var option = prompt("Choose an operation: ");
+var result = null;
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+if(option == 1)
+    {
+        console.log(result = num1 + num2);
+    }
+else if(option == 2)
+    {
+        console.log(result = num1 - num2);
+    }
+else if(option == 3)
+    {
+        console.log(result = num1 * num2);
+    }
+else if(option == 4)
+    {
+        console.log(result = num1 / num2);
+    }
+else
+    {
+        console.log("invalid option")
+    }
+
